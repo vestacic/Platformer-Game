cc8650dab5135ed7e5bca2e86f37b5667337c6f3
Vesta <vesta.cic@gmail.com>
2023-05-20 19:24:47 +0300

    add mouse and keyboard inputs

---
+++ src/Inputs/KeyboardInput.java
@@ -0,0 +1,22 @@
+package Inputs;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInput implements KeyListener {
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,11 +1,17 @@
 package Main;
 
+import Inputs.KeyboardInput;
+import Inputs.MouseInput;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
     public GamePanel(){
-
+        addKeyListener(new KeyboardInput());
+        addMouseListener(new MouseInput());
     }
     public void paintComponent(Graphics g){
         super.paintComponent(g);
