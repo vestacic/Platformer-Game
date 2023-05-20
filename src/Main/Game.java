ca3016e5e555164b2020db7c1132bec40a59f367
Vesta Cicėnaitė <vesta.cic@gmail.com>
2023-05-20 19:04:39 +0300

    create main package

---
+++ src/Main/Game.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class Game {
+    private GameWindow gameWindow;
+    private GamePanel gamePanel;
+    public Game(){
+        gamePanel = new GamePanel();
+        gameWindow = new GameWindow(gamePanel);
+    }
+}
